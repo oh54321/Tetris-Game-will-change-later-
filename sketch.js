8b9dd1b8d0c7159a6e67830a2f42d51c7504e129
oh54321 <38576852+oh54321@users.noreply.github.com>
2020-11-14 22:13:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,13 +3,23 @@ let board;
 let token1;
 let token2;
 function preload() {
-  board = loadImage('assets/board.jpg');
+  board = loadImage('assets/board.png');
   token1 = loadImage('assets/token1.png');
   token2 = loadImage('assets/token2.png');
+  w1 = loadImage('assets/wins1.png')
+  w2 = loadImage('assets/wins2.png')
+  wt = loadImage('assets/wint.png')
+  w1p = loadImage('assets/playdown1.png')
+  w2p = loadImage('assets/playdown2.png')
+  wtp = loadImage('assets/playdownt.png')
+  w1s = loadImage('assets/settingsdown1.png')
+  w2s = loadImage('assets/settingsdown2.png')
+  wts = loadImage('assets/settingsdownt.png')
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
+var isEnd = false
 var maxs = []
 var arr = []
   var mat = []
@@ -26,7 +36,110 @@ var arr = []
       maxs.push(5)
     }
 var turn = 1
-
+function rotateL(mat, maxs)
+{
+  var mat2 = []
+  var arr2 = []
+  var i
+  var j
+  for(i = 0; i < mat[0].length; i++)
+    {
+      arr2 = []
+      for(j = 0; j < mat.length; j++)
+        {
+          arr2.push(0)
+        }
+      mat2.push(arr2)
+    }
+  var ind
+  for(i = 0; i < mat.length; i++)
+    {
+      for(j = 0; j < mat.length; j++)
+        {
+          mat2[(mat.length-1)-j][i]=mat[i][j]
+        }
+    }
+  for(i = 0; i < mat.length; i++)
+    {
+      for(j = mat.length-1; j >= 0; j--)
+        {
+          mat[i][j] = 0
+        }
+    }
+  for(i = 0; i < mat.length; i++)
+    {
+      ind = 5;
+      maxs[i] = 5
+      for(j = mat.length-1; j >= 0; j--)
+        {
+          if(mat2[i][j] != 0)
+            {
+              mat[i][ind] = mat2[i][j]
+              ind--;
+              maxs[i]--;
+            }
+        }
+    }
+}
+function rotateR(mat, maxs)
+{
+  var mat2 = []
+  var arr2 = []
+  var i
+  var j
+  for(i = 0; i < mat[0].length; i++)
+    {
+      arr2 = []
+      for(j = 0; j < mat.length; j++)
+        {
+          arr2.push(0)
+        }
+      mat2.push(arr2)
+    }
+  var ind
+  for(i = 0; i < mat.length; i++)
+    {
+      for(j = 0; j < mat.length; j++)
+        {
+          mat2[j][(mat.length-1)-i]=mat[i][j]
+        }
+    }
+  for(i = 0; i < mat.length; i++)
+    {
+      for(j = mat.length-1; j >= 0; j--)
+        {
+          mat[i][j] = 0
+        }
+    }
+  for(i = 0; i < mat.length; i++)
+    {
+      ind = 5;
+      maxs[i] = 5
+      for(j = mat.length-1; j >= 0; j--)
+        {
+          if(mat2[i][j] != 0)
+            {
+              mat[i][ind] = mat2[i][j]
+              ind--;
+              maxs[i]--;
+            }
+        }
+    }
+}
+function keyPressed() {
+    var temp = checkWin(mat)
+  var i
+  if(temp == 0)
+    {
+  if (keyCode === LEFT_ARROW) {
+    rotateL(mat,maxs);
+    turn = 3-turn
+  } 
+  if (keyCode === RIGHT_ARROW) {
+    rotateR(mat,maxs);
+    turn = 3-turn
+  } }
+}
 function checkWin(arr) {
     //Columns
     for (i = 0; i < arr.length-3; i++) {
@@ -68,7 +181,16 @@ function checkWin(arr) {
             }
         }
     }
-    return 0
+  for (i = 0; i < arr.length; i++) {
+        for (j = 0; j < arr[0].length; j++) {
+            if(arr[i][j] == 0)
+            {
+              return 0
+            }  
+            }
+        }
+    
+    return 3
 }
 function checkWinSet(arr) {
     if(arr.every( v => v === arr[0])) {
@@ -79,22 +201,36 @@ function checkWinSet(arr) {
     }
 }
 function mouseClicked() {
+  var temp = checkWin(mat)
+  var i
   var s = min(windowWidth, windowHeight)/400
   var x1 = (windowWidth/s-300)/(2)
-    if(0 <= (mouseX/s-x1) <= 300 && maxs[Math.floor((mouseX/s-x1)/50)] >= 0)
+    if(temp == 0 && 0 <= (mouseX/s-x1) <= 300 && maxs[Math.floor((mouseX/s-x1)/50)] >= 0)
       {
         mat[Math.floor((mouseX/s-x1)/50)][maxs[Math.floor((mouseX/s-x1)/50)]] = turn
         maxs[Math.floor((mouseX/s-x1)/50)]--;
         turn = 3-turn;
       }
+  if(temp != 0)
+    {
+      if(mouseX/s>=x1+150-50 && x1+150+49>=mouseX/s && mouseY/s >= 102 && mouseY/s <= 132)
+        {
+          for(i = 0; i < 6; i++)
+            {
+              for(j = 0; j < 6;j++)
+                {
+                  mat[i][j] = 0
+                }
+              maxs[i] = 5
+            }
+          turn = 1
+          isEnd = false
+        }
+    }
 
 }
 
 function draw() {
-  if(checkWin(mat) != 0)
-    {
-      
-    }
   background(255,255,255);
   var s = min(windowWidth, windowHeight)/400
   var x1 = (windowWidth/s-300)/(2)
@@ -109,7 +245,7 @@ function draw() {
           if(mat[i][j] == 0)
             {
               ellipse(x1+50*i+25, 50*j+25, 40)
-              if(50*i <= (mouseX/s-x1) && (mouseX/s-x1) < 50*(i+1))
+              if(isEnd == false && 50*i <= (mouseX/s-x1) && (mouseX/s-x1) < 50*(i+1))
                 {
                   if(turn == 1)
                     {
@@ -128,6 +264,56 @@ function draw() {
           else
             {
               image(token2,x1+50*i+5, 50*j+5, 40,40)
+            }
+        }
+    }
+  var temp = checkWin(mat)
+  if(temp != 0)
+    {
+      isEnd = true
+      if(temp == 1)
+        {
+          if(mouseX/s>=x1+150-50 && x1+150+49>=mouseX/s && mouseY/s >= 102 && mouseY/s <= 132)
+            {
+              image(w1p, x1+150-125, 50, 250, 130)
+            }
+          else if(mouseX/s>=x1+150-50 && x1+150+49>=mouseX/s && mouseY/s >= 138 && mouseY/s <= 171)
+            {
+              image(w1s, x1+150-125, 50, 250, 130)
+            }
+          else
+            {
+          image(w1, x1+150-125, 50, 250, 130)
+            }
+        }
+      if(temp == 2)
+        {
+          if(mouseX/s>=x1+150-50 && x1+150+49>=mouseX/s && mouseY/s >= 102 && mouseY/s <= 132)
+            {
+              image(w2p, x1+150-125, 50, 250, 130)
+            }
+          else if(mouseX/s>=x1+150-50 && x1+150+49>=mouseX/s && mouseY/s >= 138 && mouseY/s <= 171)
+            {
+              image(w2s, x1+150-125, 50, 250, 130)
+            }
+          else
+            {
+          image(w2, x1+150-125, 50, 250, 130)
+            }
+        }
+      if(temp == 3)
+        {
+          if(mouseX/s>=x1+150-50 && x1+150+49>=mouseX/s && mouseY/s >= 102 && mouseY/s <= 132)
+            {
+              image(wtp, x1+150-125, 50, 250, 130)
+            }
+          else if(mouseX/s>=x1+150-50 && x1+150+49>=mouseX/s && mouseY/s >= 138 && mouseY/s <= 171)
+            {
+              image(wts, x1+150-125, 50, 250, 130)
+            }
+          else
+            {
+          image(wt, x1+150-125, 50, 250, 130)
             }
         }
     }
